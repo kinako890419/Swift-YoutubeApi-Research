a7a12fa93a8a515b9fb786853d9568ccf8300502
kinako890419 <61374025+kinako890419@users.noreply.github.com>
2021-08-15 20:24:55 +0800

    Add files via upload

---
+++ 20210813/lesson5/youtube-onedaybuild/youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Yuru Chen on 2021/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var model = Model()
+    var videos = [Video]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //Set itself as the datasource and the delegate
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Set itself as the delegate of the model
+        model.delegate = self
+        
+        model.getVideos()
+        
+    }
+    
+    // MARK: - Model Delegate Methods
+    
+    func videosFetched(_ videos: [Video]) {
+        
+        // Set the returned videos to our video property
+        self.videos = videos
+        
+        // Refresh the tableview
+        tableView.reloadData()
+    }
+    
+    //MARK: - TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //return how many videos we have
+        return videos.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        
+        //Configure the cell with the data
+        //Get the title for the video in question
+        let title = self.videos[indexPath.row].title
+        cell.textLabel?.text = title
+        
+        return cell
+        
+    }
+
+}
+
